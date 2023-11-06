9eaf2adf718498b6ad030762461273effe39d4be
Whimzee <pramitijagdish07@gmail.com>
2023-11-05 22:53:19 -0800

    image updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ module.exports = {
     title: 'Pramithi Jagdish',
     description:
       'Pramithi jagdish is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://pramitij.github.io.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    siteUrl: 'https://pramitij.github.io', // No trailing slash allowed!
+    image: '', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
   plugins: [
